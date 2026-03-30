a8793f9927602fdbc41c74cba347296c254be92e
EternalllZM <43221983+EternalllZM@users.noreply.github.com>
2026-03-30 09:55:23 -0500

    i'm not sure what this theme uses

--- docs/conf.py
+++ docs/conf.py
@@ -140,7 +140,7 @@ html_context = {
     "github_user": "Cog-Creators",
     "github_repo": "Red-DiscordBot",
     "github_version": "V3/develop",
-    "doc_path": "docs",
+    "conf_py_path": "/docs/",
     "version_slug": os.environ.get("READTHEDOCS_VERSION", ""),
     "rtd_language": os.environ.get("READTHEDOCS_LANGUAGE", ""),
     "READTHEDOCS": os.environ.get("READTHEDOCS", "") == "True",
